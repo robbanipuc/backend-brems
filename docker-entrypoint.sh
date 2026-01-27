2af14aae97e733d55b9dfed86b5cd9a224702675
hasan-nazmul <nh2826239@gmail.com>
2026-01-27 17:33:27 +0600

    updated seed or fake data issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 echo "Running migrations..."
 # Remember to remove :fresh --seed after the first successful deploy!
-php artisan migrate:fresh --seed --force 
+php artisan migrate --force
 echo "Starting Apache..."
 exec apache2-foreground
